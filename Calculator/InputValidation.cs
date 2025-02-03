c915a8265a75cb186c4524a6a9edf8618a328376
lixhern <andreilisai1234@gmail.com>
2025-02-03 10:52:01 +0300

    Added class for validation input info

---
+++ Calculator/InputValidation.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Calculator
+{
+    public class InputValidation
+    {
+        public static bool IsValidFio(string input, string fieldName)
+        {
+            string pattern = @"^[А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+$";
+            Regex regex = new Regex(pattern);
+
+            if (string.IsNullOrEmpty(input))
+            {
+                MessageBox.Show($"Поле '{fieldName}' не может быть пустым.");
+                return false;
+            }
+
+            if (!regex.IsMatch(input))
+            {
+                MessageBox.Show($"Введите корректное ФИО в поле '{fieldName}'.");
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool TryParseFloat(string input, out float result, string fieldName)
+        {
+            result = 0;
+            if (string.IsNullOrEmpty(input) || !float.TryParse(input, out result))
+            {
+                MessageBox.Show($"Некорректное значение в поле '{fieldName}'");
+                return false;
+            }
+            return true;
+        }
+    }
+}
